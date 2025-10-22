0686113e7f2a51e68f5cc0652db3bcbc58c0265f
ayanakhtar1 <www.ayanakhtarchachar786@gmail.com>
2025-10-22 02:20:01 -0700

    Add VCS configuration and update app version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.salmanajmal.hsk4mastery"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
